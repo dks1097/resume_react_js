f0e2b78bf264f013530757c98f5194ce234603a9
dks1097 <carlos.d.t.m19@gmail.com>
2023-06-27 14:01:05 +0100

    First Header/Navbar

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function Header() {
+  return (
+    <div className="Header">
+    </div>
+  );
+}
+
+export default Header;
+
+//WIP
--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
 import "./App.css";
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+
 
 function App() {
   return (
     <div className="App">
-      <div className="Profile"></div>
-      
       <div className="Header">
-      <div className="navbar">
-        <nav className="menu">
-          <ul className="menu-list">
-            <li>
-              <a href="#">HOME</a>
-            </li>
-            <li>
-              <a href="#">SKILLS</a>
-            </li>
-            <li>
-              <a href="#">PROJECTS</a>
-            </li>
-            <li>
-              <a href="#">EXPERIENCE</a>
-            </li>
-          </ul>
-        </nav>
+        <div className="navbar">
+          <nav className="menu">
+            <ul className="menu-list">
+              <li>
+                <a href="#">
+                  <span className="icon">
+                    <i className="fas fa-home"></i>
+                  </span>
+                  <span className="text">Home</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="icon">
+                    <i className="fas fa-puzzle-piece"></i>
+                  </span>
+                  <span className="text">Skills</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="icon">
+                    <i className="fas fa-folder"></i>
+                  </span>
+                  <span className="text">Projects</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="icon">
+                    <i className="fas fa-briefcase"></i>
+                  </span>
+                  <span className="text">Experience</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-      </div>
-
-      <footer>
-        <p>&copy; 2023 Carlos Monteiro. All rights reserved.</p>
-      </footer>
+      
     </div>
   );
 }
